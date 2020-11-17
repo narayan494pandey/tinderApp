2b02dd3e0b17f98f9b218f62902a0a3fba1ff23f
Narayan Pandey <npandey722@gmail.com>
2020-11-17 11:35:37 +0530

    logo

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import './header.css';
 import PersonIcon from '@material-ui/icons/Person'
+import IconButton from '@material-ui/core/IconBUtton'
 
 
 export default function Header() {
     return (
         <div className="header">
-        <PersonIcon />
-            
+        <IconButton>
+        <PersonIcon fontSize="large" className="header__icon" />
+        </IconButton>
+
+        <img
+            alt="logo"
+            src="https://1000logos.net/wp-content/uploads/2018/07/tinder-logo.png"
+            className="header__logo"
+        />
         </div>
     )
 }
